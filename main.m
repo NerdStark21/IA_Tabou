f07c111807fe7d65ec5da6c87713f9e659ffa10f
Aurelien Turpin <aurelien.turpin@telecom-st-etienne.fr>
2018-05-15 15:49:37 +0200

    Ajout dans le gitignore d'un test.m pour faire nos tests chacun de notre côté

--- main.m
+++ main.m
@@ -8,7 +8,3 @@ load('cities.mat');
 %coord_villes = [1 3 6 10; 2 5 8 3];
 coord_villes = [0 1 2 3; 0 3 4 5];
 chemin = [1 1 3 2];
-
-d = city_distance(chemin,coord_villes)
-
-d2 = total_distance(chemin, coord_villes)
