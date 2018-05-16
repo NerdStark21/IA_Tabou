fd72d5a7f4d862703220e19d66a92d2487f3a65a
RaphaelChevasson <rc@clubcapelli.com>
2018-05-16 09:17:21 +0200

    rectification de maj_matrice_tabou

--- maj_matrice_tabou.m
+++ maj_matrice_tabou.m
@@ -1,18 +1,20 @@
-function matrice_tabou = maj_matrice_tabou(matrice_tabou, permutation, temps_memoire_tabou)
-% fonction qui met Ã  jour la matrice tabou (contenant les permutations interdites) entre
-% deux itÃ©rations.
-% matrice_tabou : matrice_tabou(i,j) est le nbre d'itÃ©ration durant laquelle la
-%		permutation [i, j] ne peut pas Ãªtre effectuÃ©e
-% permutation : [i, j] les indices des villes permutÃ©es lors de la derniÃ¨re itÃ©ration
+function matrice_tabou = maj_matrice_tabou(matrice_tabou, permutation,...
+                                           temps_memoire_tabou)
+% fonction qui met à jour la matrice tabou (contenant les permutations
+% interdites) entre deux itérations.
+% matrice_tabou : matrice_tabou(i,j) est le nbre d'itération durant
+%     laquelle la permutation [i, j] ne peut pas être effectuée.
+% permutation : [i, j] les indices des villes permutées lors de la dernière
+%     itération
 
-%% dÃ©croissance du nombre d'itÃ©rations restant pour les autres permutations
+%% décroissance du nombre d'itérations restant pour les autres permutations
 matrice_tabou(matrice_tabou > 0) = matrice_tabou(matrice_tabou > 0) - 1;
 
-%% mise en mÃ©moire de la permutation taboue pendant temps_memoire_tabou itÃ©rations
-[i, j] = permutation
+%% mise en mémoire de la permutation taboue pendant temps_memoire_tabou itérations
+i = permutation(1)
+j = permutation(2)
+% on part du principe que i ~= j (car c'est une permutation)
 matrice_tabou(i, j) = temps_memoire_tabou;
-if i ~= j
-	matrice_tabou(j, i) = temps_memoire_tabou;
-end
+matrice_tabou(j, i) = temps_memoire_tabou;
 
 end
