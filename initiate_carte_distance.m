6e298a02be8f9963b7295892138b6c2736f67469
Aurelien Turpin <aurelien.turpin@telecom-st-etienne.fr>
2018-05-17 16:48:24 +0200

    Initialisation de la carte de distance

--- initiate_carte_distance.m
+++ initiate_carte_distance.m
@@ -1,14 +1,14 @@
-function carte_distance = initiate_carte_distance(list_coord_ville, coord_villes)
+function carte_distance = initiate_carte_distance(coord_villes)
 
 % Cette fonction renvoie la carte des distances des villes entre elles pour
 % accélerer le calcul de la distance totale d'un chemin
 
-nb_ville = size(list_coord_ville, 2);
+nb_ville = size(coord_villes, 2);
 carte_distance = zeros(nb_ville);
 
 for k=1:nb_ville
-    for j=1:nb_ville
-        carte_distance(k, j) = city_distance(
+    for j=1:k
+        carte_distance(k, j) = city_distance([k, j], coord_villes);
     end
 end
 
